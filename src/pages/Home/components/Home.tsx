30e6ab2e7b640f2742c929fb7acc191338c4a152
lamstroning <shobanov.a.a@yandex.ru>
2025-03-19 20:55:46 +0300

    Добавил диван чтобы Серега отдохнул

--- src/pages/Home/components/Home.tsx
+++ src/pages/Home/components/Home.tsx
@@ -1,16 +1,27 @@
-import { Button, HStack } from '@chakra-ui/react'
-import { RiArrowRightLine, RiMailLine } from 'react-icons/ri'
+import { Button, Card, Image, Text } from '@chakra-ui/react'
 
 const Home = () => {
   return (
-    <HStack>
-      <Button colorPalette="teal" variant="solid">
-        <RiMailLine /> Email
-      </Button>
-      <Button colorPalette="teal" variant="outline">
-        Call us <RiArrowRightLine />
-      </Button>
-    </HStack>
+    <Card.Root maxW="sm" overflow="hidden">
+      <Image
+        src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+        alt="Green double couch with wooden legs"
+      />
+      <Card.Body gap="2">
+        <Card.Title>Living room Sofa</Card.Title>
+        <Card.Description>
+          This sofa is perfect for modern tropical spaces, baroque inspired
+          spaces.
+        </Card.Description>
+        <Text textStyle="2xl" fontWeight="medium" letterSpacing="tight" mt="2">
+          $450
+        </Text>
+      </Card.Body>
+      <Card.Footer gap="2">
+        <Button variant="solid">Buy now</Button>
+        <Button variant="ghost">Add to cart</Button>
+      </Card.Footer>
+    </Card.Root>
   )
 }
 
