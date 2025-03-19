769fa013f50222f9954d60ea266c64eb31db644f
lamstroning <shobanov.a.a@yandex.ru>
2025-03-19 09:01:37 +0300

    add UI privder

--- src/root/index.tsx
+++ src/root/index.tsx
@@ -1,25 +1,16 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { ChakraProvider, createSystem, defineConfig } from '@chakra-ui/react'
 import { Provider } from 'react-redux'
 import { App } from './App'
 import { store } from './store'
+import { UIProvider } from '../components/ui/UIProvider'
 
-const config = defineConfig({
-  theme: {
-    tokens: {
-      colors: {},
-    },
-  },
-})
-
-const system = createSystem(config)
 const root = createRoot(document.getElementById('root')!)
 
 root.render(
   <Provider store={store}>
-    <ChakraProvider value={system}>
+    <UIProvider>
       <App />
-    </ChakraProvider>
+    </UIProvider>
   </Provider>
 )
